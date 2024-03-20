42dce4e2c3ddfdfa7512c47a59c8107de9600185
Asif <heyasif@asif.local>
2024-03-20 10:32:41 +0530

    register rotute added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const ConnectDatabase = require('./config/db')
 const { UserRouter } = require('./Router/User.Router')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
+
 app.use(express.json())
 app.use('/user', UserRouter)
 
